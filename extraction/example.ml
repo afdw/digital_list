1cbeb2cceed7922fccf1c6826acf9490d6bb8997
Anton Danilkin <afdw@yandex.ru>
2022-04-30 23:57:47 +0200

    Fix OCaml syntax highlighting

--- extraction/example.ml
+++ extraction/example.ml
@@ -5,6 +5,8 @@ let time l =
   flush stdout;
   v
 
+let measure_size x = (Marshal.to_bytes x [] |> Bytes.length |> float_of_int) /. 1024.0 /. 1024.0
+
 module Example = functor (Dl : Digital_list.DIGITAL_LIST) -> struct
   open Dl
 
@@ -22,8 +24,8 @@ module Example = functor (Dl : Digital_list.DIGITAL_LIST) -> struct
       for i = 0 to (!cdl |> concrete_digital_list_length r) - 1 do
         a.(i) <- !cdl |> concrete_digital_list_nth r i |> Option.get
       done;
-      Printf.printf "Raw data size (after serialization): %f MiB\n" ((Marshal.to_bytes a [] |> Bytes.length |> float_of_int) /. 1024.0 /. 1024.0);
-      Printf.printf "Data structure size (after serialization): %f MiB\n" ((Marshal.to_bytes !cdl [] |> Bytes.length |> float_of_int) /. 1024.0 /. 1024.0)
+      Printf.printf "Raw data size (after serialization): %f MiB\n" (a |> measure_size);
+      Printf.printf "Data structure size (after serialization): %f MiB\n" (cdl |> measure_size)
     ))
 end
 
